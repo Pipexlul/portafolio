b2eee298947d972b0ad37c07529cef8114d46b06
Pipexlul <felipgg@gmail.com>
2022-08-26 01:41:58 -0400

    Add title intro logic and effects

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,14 @@
 const splashScreen = document.querySelector(".splash-screen");
 const toggledBg = document.querySelector(".toggled-bg");
-const h1Text = document.querySelector("h1");
-const splashNames = document.querySelectorAll(".splash-name");
+const splashNames = document.querySelectorAll(".splash-name > span");
 
 const tileContainer = document.querySelector(".tile-container");
 const tileSize = 60;
 
 let toggled = false;
 let isAnimatingTiles = false;
+let didRolloutIntro = false;
+let didFirstClick = false;
 
 const removeAllChildren = (node) => {
 	let child = node.firstElementChild;
@@ -21,8 +22,18 @@ const toggleElemOpacity = (elem, toggleVal) => {
 	elem.style.opacity = toggleVal ? 0 : 1;
 };
 
+const firstClick = () => {
+	anime({
+		targets: splashNames,
+		easing: "linear",
+		top: "-20vh",
+		opacity: 0,
+		duration: 1000,
+	});
+};
+
 const tileClick = (ev, index, rows, columns) => {
-	if (!isAnimatingTiles) {
+	if (!isAnimatingTiles && didRolloutIntro) {
 		toggled = !toggled;
 		splashScreen.classList.toggle("toggled");
 
@@ -51,6 +62,11 @@ const tileClick = (ev, index, rows, columns) => {
 				toggledBg.style.opacity = toggled ? progress : 1 - progress;
 			},
 		});
+
+		if (!didFirstClick) {
+			firstClick();
+			didFirstClick = true;
+		}
 	}
 };
 
@@ -87,5 +103,54 @@ const createGrid = () => {
 	//console.log(`Created grid with ${rows} rows and ${columns} columns`);
 };
 
+const firstRun = () => {
+	const fadeInDelay = (index) => {
+		const bodyStyle = window.getComputedStyle(document.body);
+		const propertyName = "--fel-col-splash-text-delay" + (index + 1);
+
+		return bodyStyle.getPropertyValue(propertyName);
+	};
+
+	const delayArr = Array.from(splashNames).map((elem, idx, allElems) => {
+		return fadeInDelay(idx).trimStart();
+	});
+
+	const animation = anime.timeline({
+		easing: "linear",
+		duration: 750,
+	});
+
+	animation
+		.add(
+			{
+				targets: splashNames[0],
+				top: 0,
+				opacity: 1,
+			},
+			parseInt(delayArr[0])
+		)
+		.add(
+			{
+				targets: splashNames[1],
+				top: 0,
+				opacity: 1,
+			},
+			"+=" + delayArr[1]
+		)
+		.add(
+			{
+				targets: splashNames[2],
+				top: 0,
+				opacity: 1,
+				complete: (anim) => {
+					didRolloutIntro = true;
+				},
+			},
+			"+=" + delayArr[2]
+		);
+};
+
 createGrid();
+
 window.addEventListener("resize", createGrid);
+window.addEventListener("DOMContentLoaded", firstRun);
